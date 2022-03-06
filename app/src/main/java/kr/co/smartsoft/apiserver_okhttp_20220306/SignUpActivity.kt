18febb411261bf6272e2d6105aafa39514e1b238
이 성 노 <kulbo0926@gmail.com>
2022-03-06 15:46:21 +0900

    회원가입 요청 해서 회원 가입 신청에서 결과 확인까지 확인

--- app/src/main/java/kr/co/smartsoft/apiserver_okhttp_20220306/SignUpActivity.kt
+++ app/src/main/java/kr/co/smartsoft/apiserver_okhttp_20220306/SignUpActivity.kt
@@ -2,15 +2,40 @@ package kr.co.smartsoft.apiserver_okhttp_20220306
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.databinding.DataBindingUtil
+import kr.co.smartsoft.apiserver_okhttp_20220306.databinding.ActivitySignUpBinding
+import kr.co.smartsoft.apiserver_okhttp_20220306.utils.ServerUtil
+import org.json.JSONObject
 
 class SignUpActivity : BaseActivity() {
+    lateinit var binding:ActivitySignUpBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_sign_up)
         setupEvents()
         setValues()
     }
     override fun setupEvents() {
+        binding.btnSignUp.setOnClickListener {
+            val inputEmail = binding.edtEmailId.text.toString()
+            val inputPassword = binding.edtPassword.text.toString()
+            val inputNickname = binding.edtNickName.text.toString()
+
+            ServerUtil.putRequestSignUp(inputEmail, inputPassword, inputNickname, object : ServerUtil.JsonResponseHandler{
+                override fun onResponse(JsonObject: JSONObject) {
+                    val code = JsonObject.getInt("code")
+                    if (code == 200) {
+                        Log.d("서버응답", "가입성공")
+                    } else {
+                        Log.d("서버응답", "가입실패")
+                    }
+                }
+
+            })
+        }
+
     }
 
     override fun setValues() {
--- app/src/main/java/kr/co/smartsoft/apiserver_okhttp_20220306/utils/ServerUtil.kt
+++ app/src/main/java/kr/co/smartsoft/apiserver_okhttp_20220306/utils/ServerUtil.kt
@@ -15,6 +15,7 @@ class ServerUtil {
         val BASE_URL = "http://54.180.52.26"
         val EMAIL = "email"
         val PASSWORD = "password"
+        val NICKNAME = "nick_name"
 
         fun postRequestLogin(id:String, pw:String, handler: JsonResponseHandler ) {
 
@@ -41,6 +42,46 @@ class ServerUtil {
                 }
 
 //                인터넷은 연결되어서 응답이 정상적으로 온 경우
+                override fun onResponse(call: Call, response: Response) {
+                    val bodyString = response.body!!.string()
+//                  응답의 본문을 string으로 변환하면 json 적용된 상태
+
+                    val jsonObj = JSONObject(bodyString)
+                    Log.d("서버응답 : ", jsonObj.toString())
+                    handler?.onResponse(jsonObj)
+
+                }
+
+            } )
+
+
+        }
+        fun putRequestSignUp(id:String, pw:String, nick:String, handler: JsonResponseHandler ) {
+
+            val urlString = "${BASE_URL}/user"
+
+            val formData = FormBody.Builder()
+                .add(EMAIL, id)
+                .add(PASSWORD, pw)
+                .add(NICKNAME, nick)
+                .build()
+
+            val request = Request.Builder()
+                .url(urlString)
+                .put(formData)
+                .build()
+
+            val client = OkHttpClient()
+
+//            OkHttpClient 객체를 이용 -> 서버에 로그인 기능 실제 호출
+//            호출을 했으면 서버가 수행할 결과를 받아서 처리
+//            => 서버에 다녀와서 할 일을 등록 : enqueue(Callback)
+            client.newCall(request).enqueue(object : Callback {
+                override fun onFailure(call: Call, e: IOException) {
+//                인터넷의 연결이 안되어서 또는 서버 다운 등으로  정상적인 연결이 안된경우
+                }
+
+                //                인터넷은 연결되어서 응답이 정상적으로 온 경우
                 override fun onResponse(call: Call, response: Response) {
                     val bodyString = response.body!!.string()
 //                  응답의 본문을 string으로 변환하면 json 적용된 상태
